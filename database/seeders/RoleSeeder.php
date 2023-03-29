294c230536316fbc7e585e041ace29a63f1a5d0a
Rinanda <rinandasoe@gmail.com>
2023-03-29 10:07:25 +0700

    modif seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Role::create([
+            'nama' => 'management',
+        ]);
+
+        Role::create([
+            'nama' => 'sales',
+        ]);
+
+        Role::create([
+            'nama' => 'accounting',
+        ]);
     }
 }
