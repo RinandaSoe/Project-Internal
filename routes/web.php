0fbb693a8a8943d530ff18632b08d54d32a9fa39
infinite <infinite@ib-synergy.co.id>
2023-03-31 16:49:57 +0700

    commit SOPO-28 SOPO-29

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+
+// Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('forms', 'forms')->name('forms');
+    Route::view('cards', 'cards')->name('cards');
+    Route::view('charts', 'charts')->name('charts');
+    Route::view('buttons', 'buttons')->name('buttons');
+    Route::view('modals', 'modals')->name('modals');
+    Route::view('tables', 'tables')->name('tables');
+    Route::view('calendar', 'calendar')->name('calendar');
+// });
+
 // Route::resource('/home' ,PartController::class);
